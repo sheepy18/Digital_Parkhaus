c814db6c3eb46f49b45248abe37877208f6e4beb
sheepy18 <32834956+sheepy18@users.noreply.github.com>
2017-10-16 21:06:56 +0200

    HelloWorld with JUnit
    First and little program with JUnit

---
+++ JavaCode_Parkhaus/src/Test/HelloWorld.java
@@ -0,0 +1,11 @@
+package Test;
+
+/**
+ * Created by Lukas on 16.10.2017.
+ */
+public class HelloWorld {
+
+    public String greetings(String name) {
+        return "Hello " + name;
+    }
+}
---
+++ JavaCode_Parkhaus/src/Test/HelloWorldTest.java
@@ -0,0 +1,18 @@
+package Test;
+
+/**
+ * Created by Lukas on 16.10.2017.
+ */
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+    HelloWorld greeter = new HelloWorld();
+
+    @Test
+    public void test() {
+        assertEquals("Hello World", greeter.greetings("World"));
+    }
+}
