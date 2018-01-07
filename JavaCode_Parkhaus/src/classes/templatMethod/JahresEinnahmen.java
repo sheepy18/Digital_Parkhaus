b6101dd72f1d8521e8bf8cbc4a0e8989d0cc1be8
DESKTOP-5CUBI1D\Sheepy <Sheepy@gmx.net>
2018-01-07 16:27:16 +0100

    JahresEinkommen

--- JavaCode_Parkhaus/src/classes/templatMethod/JahresEinnahmen.java
+++ JavaCode_Parkhaus/src/classes/templatMethod/JahresEinnahmen.java
@@ -6,6 +6,8 @@ import java.util.Map;
 public class JahresEinnahmen extends Einnahmen {
     @Override
     double getSpecificEinnahmen(Map<Calendar, Float> einnahmen, Calendar current) {
-        return 0;
+        return einnahmen.keySet().stream()
+                .filter(c -> c.get(Calendar.YEAR) == current.get(Calendar.YEAR))
+                .mapToDouble(c -> einnahmen.get(c)).sum();
     }
 }
