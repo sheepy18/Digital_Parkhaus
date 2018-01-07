1f81e2296315108068c30c02f2a92b6acbeb4743
DESKTOP-5CUBI1D\Sheepy <Sheepy@gmx.net>
2018-01-07 16:26:22 +0100

    MonatsEinkommen

--- JavaCode_Parkhaus/src/classes/templatMethod/MonatsEinnahmen.java
+++ JavaCode_Parkhaus/src/classes/templatMethod/MonatsEinnahmen.java
@@ -6,6 +6,9 @@ import java.util.Map;
 public class MonatsEinnahmen extends Einnahmen  {
     @Override
     double getSpecificEinnahmen(Map<Calendar, Float> einnahmen, Calendar current) {
-        return 0;
+        return einnahmen.keySet().stream()
+                .filter(c -> c.get(Calendar.YEAR) == current.get(Calendar.YEAR)
+                        && c.get(Calendar.MONTH) == current.get(Calendar.MONTH))
+                .mapToDouble(c -> einnahmen.get(c)).sum();
     }
 }
